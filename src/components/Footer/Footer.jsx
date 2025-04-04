4d9f58f920cd506263f18a4d480c92c04f714e9b
Pedro Vinícius <pedromichelwork@gmail.com>
2025-04-03 21:41:22 -0300

    inserting href FACEBOOK

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import styles from "./Footer.module.css"; // Para estilização
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>WORKING ON</p>
+      <p>&copy; {new Date().getFullYear()} Meu Site. Todos os direitos reservados.</p>
       <nav>
-        <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
-        <a href="https://www.instagram.com/ieediomicio/
-"
-        target="_blank" 
-        rel="noopener noreferrer">Instagram
-</a>
+        <a href="/sobre">Sobre</a> |
+        <a href="https://www.instagram.com/ieediomicio/"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Instagram">
+        <FaInstagram/></a>
+        <a href="https://www.facebook.com/p/IEE-Diom%C3%ADcio-Freitas-100066695940896/?locale=pt_BR&_rdr"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Facebook">
+          <FaFacebookF/></a>
       </nav>
     </footer>
   );
