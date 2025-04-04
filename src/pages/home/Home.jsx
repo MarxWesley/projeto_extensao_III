2ede8bfab43dd2394b24c03e2fe863df45a294d1
Wesley Marques <Marques_bertin@hotmail.com>
2025-04-03 22:39:50 -0300

    adicionado umas imagens

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import styles from './Home.module.css'
+import img_pessoas from '../../assets/img/WhatsApp Image 2025-03-10 at 14.31.26 (1).jpeg'
 
 function Home() {
     return (
-        <section className="">
-            <img src="/" alt='IEE' />
+        <section className={styles.section}>
+            <img className={styles.img} src={img_pessoas} alt='IEE' />
         </section>
     )
 }
