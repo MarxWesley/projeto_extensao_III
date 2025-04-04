6a12ae5eeeee59797506edf9e77a329078943a1f
Wesley Marques <Marques_bertin@hotmail.com>
2025-04-03 22:27:47 -0300

    comitando todas as mudancas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,28 +5,30 @@ import { FaFacebookF, FaInstagram, FaMapMarkerAlt, FaPhone } from "react-icons/f
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Meu Site. Todos os direitos reservados.</p>
+      <p>&copy; {new Date().getFullYear()}
+      <a href="/sobre">Sobre</a>
+      </p>
       <nav>
-        <a href="/sobre">Sobre</a> |
         <a href="https://www.instagram.com/ieediomicio/"
         target="_blank"
         rel="noopener noreferrer"
         title="Instagram">
         <FaInstagram/></a>
 
-        <a href="https://www.facebook.com/p/IEE-Diom%C3%ADcio-Freitas-100066695940896/?locale=pt_BR&_rdr"
+        <a href="https://www.facebook.com/p/IEE-Diomício-Freitas-100066695940896/?locale=pt_BR&_rdr"
           target="_blank"
           rel="noopener noreferrer"
           title="Facebook">
-          <FaFacebookF/></a>
+          <FaFacebookF/>  </a>
+          <FaPhone/> (48) 3433-8235
         </nav>
 
         <div className={styles.contactInfo}>
           <p><FaMapMarkerAlt/> R. Lúcia Milioli 211, Santa Bárbara, Criciúma - SC </p>
-          <p><FaPhone/> (48) 3433-8235</p>
+
         </div>
     </footer>
   );
 };
 
-export default Footer;
+export default Footer
