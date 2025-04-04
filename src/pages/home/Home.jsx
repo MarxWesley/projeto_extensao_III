ecd16c469e74b736375546442dbe0f750a6fda30
Wesley Marques <Marques_bertin@hotmail.com>
2025-04-03 23:05:57 -0300

    mudancas

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import styles from './Home.module.css'
-import img_pessoas from '../../assets/img/pessoas_iee.jpeg'
+import img_pessoas from '../../assets/img/pessoas.png'
 
 function Home() {
     return (
         <section className={styles.section}>
-            <img className={styles.img} src={img_pessoas} alt='IEE' />
+            <div className={styles.container}>
+                <img className={styles.img} src={img_pessoas} alt='IEE' />
+            </div>
         </section>
     )
 }
