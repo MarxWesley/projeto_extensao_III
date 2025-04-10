e96cfcdda27d4be7b041dac236799eab27b9eb31
Laboratório 05 <lab05@esucri.edu>
2025-04-10 19:14:10 -0400

    carousel created

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,26 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import styles from './Carousel.module.css'
+
+const Carousel = ({img1, img2, img3,alt}) => {
+  const settings = {
+    dots: true,          // Exibe os pontinhos
+    infinite: true,      // Carrossel infinito
+    speed: 500,          // Velocidade de transição
+    slidesToShow: 1,     // Exibe um slide por vez
+    slidesToScroll: 1,   // Avança um slide por vez
+    autoplay: true,      // Ativa o autoplay
+    autoplaySpeed: 5000, // Tempo entre as trocas de slides (em milissegundos)
+  };
+
+  return (
+    <Slider {...settings}>
+      <div><img className={styles.banner_img} src={img1} alt={alt}/></div>
+      <div><img className={styles.banner_img} src={img2} alt={alt}/></div>
+      <div><img className={styles.banner_img} src={img3} alt={alt}/></div>
+    </Slider>
+  );
+};
+
+export default Carousel
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import styles from './Home.module.css'
 import Notice from '../../components/Notice/Notice'
 import Section from '../../components/Section/Section'
+import Carousel from '../../components/Carousel/Carousel'
+import entrada from '../../assets/img/Entrada.jpeg'
 
 function Home() {
     return (
         <section className={styles.section}>
+            <Carousel img1={entrada} img2={entrada} img3={entrada} alt="Imagens do Banner"/>
             <Section />
             <Notice />
         </section>
