055a47f3ea88d286cb8f55beae1e4da96075dbf4
Pedro Vinícius <pedromichelwork@gmail.com>
2025-04-21 18:02:03 -0300

    starting changes

--- src/pages/home/howToHelp/HowToHelp.jsx
+++ src/pages/home/howToHelp/HowToHelp.jsx
@@ -5,13 +5,15 @@ import qrCodeImg from '../../../assets/img/qrcode-pix.png';
 function HowToHelp() {
   return (
     <div className="how-to-help-container">
-      <h1>Como Ajudar</h1>
+        
+      <h1>Como Ajudar?</h1>
       <p>
         Você pode contribuir com nosso projeto fazendo uma doação via PIX.
         Basta escanear o QR Code abaixo:
-      </p>
+    </p>
       <img src={qrCodeImg} alt="QR Code para doação via PIX" className="qr-code" />
       <p>Chave PIX: <strong>exemplo@email.com</strong></p>
+      
     </div>
   );
 }
