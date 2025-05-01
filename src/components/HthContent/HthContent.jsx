8e145beb91e1f1a99ac82fea95d7f04be8ba3995
Pedro Vinícius <pedromichelwork@gmail.com>
2025-05-01 16:50:15 -0300

    changing css

--- src/components/HthContent/HthContent.jsx
+++ src/components/HthContent/HthContent.jsx
@@ -1,17 +1,15 @@
 import styles from './HthContent.module.css'
 
-export default function HthContent({title1, title2, content1, content2, buttonText}) {
+export default function HthContent({title, content, buttonText}) {
     return (
         <div className={styles.card}>
                 <div className={styles.icon}></div>
                 <div className={styles.content}>
-                    <h2 className={styles.cardTitle}>{title1}</h2>
+                    <h2 className={styles.cardTitle}>{title}</h2>
                     <p className={styles.cardText}>
-                        {content1}
+                        {content}
                     </p>
                     <button className={styles.button}>{buttonText}</button> <br /> <br />
-                    <h2 className={styles.cardTitle}>{title2}</h2>
-                    <p className={styles.cardText}>{content2}</p>
                 </div>
             </div>
     );
--- src/pages/howToHelp/HowToHelp.jsx
+++ src/pages/howToHelp/HowToHelp.jsx
@@ -2,6 +2,25 @@ import HthContent from '../../components/HthContent/HthContent';
 import styles from './HowToHelp.module.css';
 
 function HowToHelp() {
+
+    const contents = [
+        {
+            title: "Fazer um Pix para o instituto",
+            content: "O Pix é uma forma rápida de ajudar o instituto Diomício Freitas em suas diversas demandas financeiras. Clique no botão abaixo e doe agora mesmo!",
+            buttonText: "Copiar Chave"
+        },
+        {
+            title: "Realizar uma doação online",
+            content: "A doação online é uma das formas mais práticas de ajudar. Basta clicar no botão abaixo e doar o valor que quiser agora mesmo!",
+            buttonText: "Doe pelo PayPal Chave"
+        },
+        {
+            title: "Doar produtos e serviços",
+            content: "A Viver precisa de diversos produtos e serviços em seu dia-a-dia, e você ou sua empresa podem colaborar doando-os. "
+        }
+
+    ] 
+
     return (
         <section className={styles.section}>
             <h1 className={styles.title}>Faça parte desta história de amor ao próximo!</h1>
@@ -9,14 +28,12 @@ function HowToHelp() {
                 Para apoiar a Viver e as crianças e adolescentes beneficiados pela instituição, você pode:
             </p>
 
-            
+            { contents.length > 0 && contents.map((content) =>
             <HthContent 
-                title1={"Fazer um Pix para o instituto"}
-                buttonText={"Copiar Chave"}
-                content1={"O Pix é uma forma rápida de ajudar o instituto Diomício Freitas em suas diversas demandas financeiras. Clique no botão abaixo e doe agora mesmo!"}
-                title2={"Realizar uma doação online"}
-                content2={"A doação online é uma das formas mais práticas de ajudar. Basta clicar no botão abaixo doar o valor que quiser agora mesmo!"}
-            /> 
+                title={content.title}
+                buttonText={content.buttonText}
+                content={content.content}
+            /> )}
         
         </section>
     );
