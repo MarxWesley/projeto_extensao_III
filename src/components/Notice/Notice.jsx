7457ad8683255768faa2b61c32d5bb747c19f05a
Wesley Marques <Marques_bertin@hotmail.com>
2025-06-25 20:50:07 -0300

    fix: notice

--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -3,6 +3,7 @@ import NewNotice from '../newNotice/NewNotice'
 import NoticeCard from '../NoticeCard/NoticeCard'
 import styles from './Notice.module.css'
 import { useEffect, useState } from 'react'
+import img from './teste_1.png'
 
 function Notice() {
 
@@ -19,7 +20,6 @@ function Notice() {
         .then(resp => resp.json())
         .then((data) => {
             const filterData = data
-            .filter(item => item.status === true) 
             .map(item => {
                 const dataOriginal = new Date(item.createdAt);
                 const newDate = dataOriginal.toLocaleDateString('pt-BR', {
@@ -39,28 +39,35 @@ function Notice() {
     }, [])
  
     return (
-        <div className={styles.container}>
-            {notice.length > 0 && notice.map((noticia) => 
-                <div key={noticia.id} className={styles.container_img}> 
-                    <NoticeCard
-                        date={noticia.createdAt}
-                        id={noticia.id}
-                        img={noticia.img}
-                        alt={noticia.title}
-                        title={noticia.title}
-                        link={noticia.link}
-                        text_link={'Leia mais'}
+        <>
+            {notice.length > 0 ? (
+                <div className={styles.container}>
+                    {notice.map((noticia) =>
+                    <div key={noticia.id} className={styles.container_img}>
+                        <NoticeCard
+                            date={noticia.createdAt}
+                            id={noticia.id}
+                            img={noticia.img}
+                            alt={noticia.title}
+                            title={noticia.title}
+                            link={noticia.link}
+                            text_link={'Leia mais'}
+                        />
+                    </div>
+                    )}
+                    {token && 
+                        <NewNotice />
+                    }
+                </div>
+                ) : (
+                <div className={styles.containerNoNotice}>
+                    <img 
+                        src={img} 
+                        alt="Imagem para quando está sem notícia" 
                     />
                 </div>
-            )}
-            {
-                token && (
-                    <>
-                        <NewNotice/>
-                    </>
-                )
-            }
-        </div>
+                )}
+        </>
     )
 }
 
